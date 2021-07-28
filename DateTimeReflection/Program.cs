c7b2c5d6dab11691da88e10fccc6d53382445c56
Матвей Волков <flexbookmatvey@icloud.com>
2021-07-28 17:20:26 +0500

    Добавьте файлы проекта.

---
+++ DateTimeReflection/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Reflection;
+
+namespace DateTimeReflection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Type dateType = typeof(DateTime);
+            PropertyInfo[] dateTypeProperties =  dateType.GetProperties();
+            foreach(PropertyInfo property in dateTypeProperties)
+            {
+                Console.WriteLine($"{property.Name} - {property.PropertyType}");
+            }
+        }
+    }
+}
